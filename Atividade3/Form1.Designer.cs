f64a1705f698d676243990d57ff588b0f30a085d
Guilherme Henrique <guilherme.almeida129@etec.sp.gov.br>
2020-09-25 17:45:36 -0300

    Calculadora de peso ideal

---
+++ Atividade3/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Atividade3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rdbtnMasc = new System.Windows.Forms.RadioButton();
+            this.rdbtnFem = new System.Windows.Forms.RadioButton();
+            this.msktxtAlt = new System.Windows.Forms.MaskedTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rdbtnMasc
+            // 
+            this.rdbtnMasc.AutoSize = true;
+            this.rdbtnMasc.Location = new System.Drawing.Point(6, 19);
+            this.rdbtnMasc.Name = "rdbtnMasc";
+            this.rdbtnMasc.Size = new System.Drawing.Size(73, 17);
+            this.rdbtnMasc.TabIndex = 0;
+            this.rdbtnMasc.TabStop = true;
+            this.rdbtnMasc.Text = "Masculino";
+            this.rdbtnMasc.UseVisualStyleBackColor = true;
+            // 
+            // rdbtnFem
+            // 
+            this.rdbtnFem.AutoSize = true;
+            this.rdbtnFem.Location = new System.Drawing.Point(6, 42);
+            this.rdbtnFem.Name = "rdbtnFem";
+            this.rdbtnFem.Size = new System.Drawing.Size(67, 17);
+            this.rdbtnFem.TabIndex = 1;
+            this.rdbtnFem.TabStop = true;
+            this.rdbtnFem.Text = "Feminino";
+            this.rdbtnFem.UseVisualStyleBackColor = true;
+            // 
+            // msktxtAlt
+            // 
+            this.msktxtAlt.Location = new System.Drawing.Point(64, 6);
+            this.msktxtAlt.Mask = "0.00";
+            this.msktxtAlt.Name = "msktxtAlt";
+            this.msktxtAlt.Size = new System.Drawing.Size(27, 20);
+            this.msktxtAlt.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdbtnMasc);
+            this.groupBox1.Controls.Add(this.rdbtnFem);
+            this.groupBox1.Location = new System.Drawing.Point(21, 32);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(93, 70);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sexo";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Altura:";
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Location = new System.Drawing.Point(61, 105);
+            this.txtPeso.MaxLength = 6;
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(40, 20);
+            this.txtPeso.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Peso:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(107, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "KG";
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(12, 132);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(119, 23);
+            this.btnCalc.TabIndex = 8;
+            this.btnCalc.Text = "Calcular peso ideal";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(143, 167);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPeso);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.msktxtAlt);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rdbtnMasc;
+        private System.Windows.Forms.RadioButton rdbtnFem;
+        private System.Windows.Forms.MaskedTextBox msktxtAlt;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPeso;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnCalc;
+    }
+}
+
---
+++ Atividade3/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Atividade3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        double num;
+        double peso;
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            if (rdbtnMasc.Checked) {
+                try
+                {
+                    num = 72.7 * double.Parse(msktxtAlt.Text) - 58;
+                    
+                }
+                catch
+                {
+                    MessageBox.Show("Insira valores válidos!");
+                }
+            }
+            else if(rdbtnFem.Checked){
+                try
+                {
+                    num = 62.1 * Double.Parse(msktxtAlt.Text) - 44.7;
+                }
+                catch
+                {
+                    MessageBox.Show("Insira valores válidos!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Selecione o sexo!");
+            }
+
+            try
+            {
+                peso = Double.Parse(txtPeso.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Insira um peso válido!");
+            }
+            try
+            {
+                if (Math.Round(num, 0) > double.Parse(txtPeso.Text))
+                {
+                    MessageBox.Show("Coma bastante massas e doces");
+                }
+                else if (Math.Round(num, 0) < double.Parse(txtPeso.Text))
+                {
+                    MessageBox.Show("Regime obrigatório já");
+                }
+                else
+                {
+                    MessageBox.Show("Você está no peso ideal");
+                }
+            } catch
+            {
+                MessageBox.Show("Insira valores válidos!");
+            }
+        }
+    }
+}
