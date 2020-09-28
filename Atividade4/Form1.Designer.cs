d5dd64836046eadfeada4072253cb6143c113644
Guilherme Henrique <guilherme.almeida129@etec.sp.gov.br>
2020-09-28 00:03:01 -0300

    Tamanho da janela fixado

--- Atividade4/Form1.Designer.cs
+++ Atividade4/Form1.Designer.cs
@@ -118,7 +118,10 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Verificador";
             this.ResumeLayout(false);
             this.PerformLayout();
